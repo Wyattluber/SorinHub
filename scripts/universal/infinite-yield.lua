cf5f559dc7a146cac94d16494c9f37c30da40dd8
Wyattluber <81474359+Wyattluber@users.noreply.github.com>
2025-07-31 02:20:28 +0200

    Create infinite-yield.lua
    Add Infinite Yield script

---
+++ scripts/universal/infinite-yield.lua
@@ -0,0 +1,9 @@
+-- Infinite Yield Admin Commands
+-- SorinHub Integration
+-- Author: EdgeIY
+-- Version: 5.12.1
+
+print("🚀 Loading Infinite Yield from SorinHub...")
+
+-- Lade das originale Infinite Yield Script
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
