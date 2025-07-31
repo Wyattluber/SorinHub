698cb04bbf9ea87ce6a71228e4f119cd83392d2a
Wyattluber <81474359+Wyattluber@users.noreply.github.com>
2025-07-31 02:22:02 +0200

    Create dark-dex.lua
    Add Dark Dex script

---
+++ scripts/universal/dark-dex.lua
@@ -0,0 +1,9 @@
+-- Dark Dex Explorer
+-- SorinHub Integration
+-- Author: Moon
+-- Version: 3.0.0
+
+print("🚀 Loading Dark Dex from SorinHub...")
+
+-- Lade das originale Dark Dex Script
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua", true))()
