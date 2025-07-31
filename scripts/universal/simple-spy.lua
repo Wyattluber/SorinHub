eb7d727257c6c6b120e676bfb858271b97dd1e76
Wyattluber <81474359+Wyattluber@users.noreply.github.com>
2025-07-31 02:22:57 +0200

    Create simple-spy.lua
    Add Simple Spy script

---
+++ scripts/universal/simple-spy.lua
@@ -0,0 +1,9 @@
+-- Simple Spy Remote Logger
+-- SorinHub Integration
+-- Author: exx
+-- Version: 2.0.0
+
+print("🚀 Loading Simple Spy from SorinHub...")
+
+-- Lade das originale Simple Spy Script
+loadstring(game:HttpGet("https://raw.githubusercontent.com/exxtremestuffs/SimpleSpySource/master/SimpleSpy.lua",true))()
